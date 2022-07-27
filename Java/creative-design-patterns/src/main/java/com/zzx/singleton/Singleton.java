b1a7ea155a31963fb11be7aa99a46137a1e34162
PermabanSun <295375895@qq.com>
2022-07-27 16:13:15 +0800

    优化懒汉式单例

--- Java/creative-design-patterns/src/main/java/com/zzx/singleton/Singleton.java
+++ Java/creative-design-patterns/src/main/java/com/zzx/singleton/Singleton.java
@@ -11,7 +11,7 @@ package com.zzx.singleton;
 public class Singleton {
 
     private final static Singleton HUNGRY_STYLE_INSTANCE = new Singleton();
-    private static Singleton LAZY_STYLE_INSTANCE;
+    private static volatile Singleton LAZY_STYLE_INSTANCE;
 
     private Singleton() {}
 
@@ -28,9 +28,15 @@ public class Singleton {
      * synchronized 避免线程安全问题
      * @return Singleton
      */
-    public static synchronized Singleton getLazyStyleInstance() {
+    public static Singleton getLazyStyleInstance() {
         if (LAZY_STYLE_INSTANCE == null) {
-            LAZY_STYLE_INSTANCE = new Singleton();
+            // 将 synchronized 加在这一步上，优化效率
+            // 但这样又会有问题，因为这样还是有可能多个线程同时判断为 null 而进入等锁的状态
+            synchronized (Singleton.class) {
+                // 内层还要进行一次检查，双重检查锁定
+                if (LAZY_STYLE_INSTANCE == null)
+                    LAZY_STYLE_INSTANCE = new Singleton();
+            }
         }
         return LAZY_STYLE_INSTANCE;
     }
