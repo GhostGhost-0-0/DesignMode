505de9a611551ba8c736adf2679f9e033bee17d2
PermabanSun <295375895@qq.com>
2022-07-29 15:47:59 +0800

    补充了类注释

--- Java/structural-design-patterns/src/main/java/com/zzx/adapter/TestAdapter.java
+++ Java/structural-design-patterns/src/main/java/com/zzx/adapter/TestAdapter.java
@@ -5,7 +5,7 @@ package com.zzx.adapter;
  * @BelongPackage: com.zzx.adapter
  * @Author: 那个小楠瓜
  * @CreateTime: 2022-07-29 15:44
- * @Description: TODO
+ * @Description: 手机供应商适配器
  * @Version: 1.0
  */
 public class TestAdapter extends TestSupplier implements Target{
